330e32430e42f454028082bdf58ef3135ae998e5
Fernando Martínez <fernando_m1911a1@hotmail.com>
2021-08-11 01:52:38 -0500

    Adds styling to components

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Filter } from './Filter'
 import { List } from './List';
+import './App.css'
 
 export const App = () => {
   return (
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import './Filter.css'
 
 export const Filter = () => {
   return (
-    <>
+    <div id="filter">
       <div>
         <input type="text" placeholder="Type to filter..."></input>
       </div>
       <div>
         <button type="button">View Favorites</button>
       </div>
-    </>
+    </div>
   )
 }
